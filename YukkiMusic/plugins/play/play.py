717e41f67f3ac5d0b6258a887137c572d188e432
med-drg <111098649+med-drg@users.noreply.github.com>
2022-08-13 04:29:33 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -19,6 +19,7 @@ from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
+from strings.filters import command
 from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
 from YukkiMusic.core.call import Yukki
