e475e12d01587fcb16c19f333bc1937b7873de3d
jdjdhdjdjdjdj <109516184+jdjdhdjdjdjdj@users.noreply.github.com>
2022-08-14 00:03:16 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -42,7 +42,7 @@ force_btn = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(
-                text="اشترك هنا", url="https://t.me/معرف قناتك بدون @"
+                text="اشترك هنا", url="https://t.me/U_K_M8 @"
             ),                        
         ],        
     ]
@@ -51,10 +51,10 @@ force_btn = InlineKeyboardMarkup(
 async def check_is_joined(message):    
     try:
         userid = message.from_user.id
-        status = await app.get_chat_member("معرف قناتك بدون @", userid)
+        status = await app.get_chat_member("U_K_M8 @", userid)
         return True
     except Exception:
-        await message.reply_text("عذرا ؏ُـمريـہ أنت غير مشترك في @YY8GG ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        await message.reply_text("عذرا ؏ُـمريـہ أنت غير مشترك في @U_K_M8 ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
         return False
 
 # Command
