c4d1d0be61a211444eac744ad4f8a8cb30eef13a
med-drg <111098649+med-drg@users.noreply.github.com>
2022-08-13 17:50:22 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -53,7 +53,7 @@ async def check_is_joined(message):
         status = await app.get_chat_member("yy8gg", userid)
         return True
     except Exception:
-        await message.reply_text("عذرا ؏ُـمريـہ أنت غير مشترك في @pla_m1 ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        await message.reply_text("عذرا ؏ُـمريـہ أنت غير مشترك في @YY8GG** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
         return False
 
 # Command
