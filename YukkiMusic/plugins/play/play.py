06f8db72516cbcd7fb9d2cee8001a3222a80a1b8
med-drg <111098649+med-drg@users.noreply.github.com>
2022-08-13 17:00:55 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -35,6 +35,25 @@ from YukkiMusic.utils.inline.playlist import botplaylist_markup
 from YukkiMusic.utils.logger import play_logs
 from YukkiMusic.utils.stream.stream import stream
 
+force_btn = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                text="اشترك هنا", url="https://t.me/yy8gg"
+            ),                        
+        ],        
+    ]
+)
+
+async def check_is_joined(message):    
+    try:
+        userid = message.from_user.id
+        status = await app.get_chat_member("yy8gg", userid)
+        return True
+    except Exception:
+        await message.reply_text("عذرا ؏ُـمريـہ أنت غير مشترك في @YY8GG ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        return False
+
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
